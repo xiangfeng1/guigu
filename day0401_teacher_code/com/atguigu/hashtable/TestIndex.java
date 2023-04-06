8c48163da7ac0b92556a3c0c0911bfa525012a5f
zxf <100327251+xiangfeng1@users.noreply.github.com>
2023-04-06 16:39:59 +0800

    Delete day0401_teacher_code directory

--- day0401_teacher_code/com/atguigu/hashtable/TestIndex.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.hashtable;
-
-public class TestIndex {
-    public static void main(String[] args) {
-        System.out.println(31 & 15);//15
-        System.out.println(63 & 15);//15
-    }
-}
